109e0d055f5a01aa603dc50b5d71c11317b1bf88
Kamil Essekkat <kamil.essekkat@acaisoft.com>
2021-03-14 21:23:21 +0100

    Remove unused annotations

--- src/WorkerWithShiftsDTO.kt
+++ src/WorkerWithShiftsDTO.kt
@@ -2,9 +2,7 @@ package pl.essekkat
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import java.time.LocalDateTime
 
 
@@ -16,10 +14,8 @@ data class WorkerDTO(
 
 data class ShiftDTO(
     @JsonDeserialize(using = LocalDateTimeDeserializer::class)
-//    @JsonSerialize(using = LocalDateTimeSerializer::class)
     val start: LocalDateTime,
     @JsonDeserialize(using = LocalDateTimeDeserializer::class)
-//    @JsonSerialize(using = LocalDateTimeSerializer::class)
     val end: LocalDateTime
 )
 
